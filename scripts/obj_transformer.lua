6dd109cd49901ea825ef5eb13d57f5763370fd93
Miguel Carvalho <m.a.a.carvalho@outlook.com>
2021-12-20 20:23:33 +0000

    Stripped functionality from the .obj transformer script.

--- scripts/obj_transformer.lua
+++ scripts/obj_transformer.lua
@@ -2,7 +2,6 @@
 OBJ_PATH = nil
 
 -- Conversion Parameters
-SPLIT_OBJ         = true
 CONVERT_NORMALS   = true
 CONVERT_MATERIALS = true
 
@@ -153,152 +152,6 @@ stripObj = function ()
 
 end
 
--- Converts .obj to several singular purpose files
-splitObj = function ()
-
-    -- Importing .obj & .mtl loader
-    local obj_loader = require "lib/obj_loader"
-    local mtl_loader = require "lib/mtl_loader"
-
-    FD_TO = io.open(OBJ_PATH..TO_EXT, "w+")
-
-    for parsed_line in obj_loader.load(OBJ_PATH, OBJ_V_TYPES) do
-
-        local type = parsed_line.type
-
-        if type == "v" then
-
-            -- Writing string to file
-            FD_TO:write(parsed_line.original, '\n')
-
-        elseif type == "vt" and CONVERT_MATERIALS then
-
-            -- Writing string to file
-            FD_TO:write(parsed_line.original, '\n')
-
-        elseif type == "vn" and CONVERT_NORMALS then
-
-            -- Writing string to file
-            FD_TO:write(parsed_line.original, '\n')
-
-        end
-    end
-
-    FD_TO:close()
-
-    for parsed_line in obj_loader.load(OBJ_PATH, OBJ_F_TYPES) do
-
-        local type = parsed_line.type
-
-        if type == "mtllib" then
-
-            -- Processing .mtl file
-            for mtl_line in mtl_loader.load(parsed_line.file, MTL_TYPES) do
-
-                if mtl_line.type == "newmtl" then
-
-                    -- Updating current material
-                    CUR_MATERIAL = mtl_line.m
-                    -- Storing current material index
-                    MATERIALS[CUR_MATERIAL] = {idx = MATERIALS_IDX, lib = parsed_line.original, written = false}
-                    -- Incrementing the index for the next material
-                    MATERIALS_IDX = MATERIALS_IDX + 1
-
-                elseif mtl_line.type == "Kd" then
-
-                    MATERIALS[CUR_MATERIAL].Kd = {
-                        r = mtl_line.r,
-                        g = mtl_line.g,
-                        b = mtl_line.b
-                    }
-
-                elseif mtl_line.type == "map_Kd" then
-
-                    MATERIALS[CUR_MATERIAL].map_Kd = mtl_line.file
-
-                end
-
-            end
-
-            -- Opening .materials file
-            FD_MT = io.open(OBJ_PATH..MT_EXT, "w+")
-
-        elseif type == "usemtl" then
-
-            -- Storing current material
-            CUR_MATERIAL = parsed_line.m
-
-            -- Getting current material's properties
-            local m = MATERIALS[CUR_MATERIAL]
-
-            if not m.written then
-
-                -- Writing material to .materials file
-                FD_MT:write(string.format("%03d", m.idx).." " ..CUR_MATERIAL.." "..m.Kd.r.." "..m.Kd.g.." "..m.Kd.b)
-                if m.map_Kd ~= nil then
-                    FD_MT:write(" "..m.map_Kd)
-                end
-                FD_MT:write("\n")
-
-                -- Creating .obj for this material
-                os.execute("cp "..OBJ_PATH..TO_EXT.." "..OBJ_PATH..string.format(".%03d", m.idx)..O_EXT)
-
-                if FD_O ~= nil then FD_O:close() end
-                FD_O = io.open(OBJ_PATH..string.format(".%03d", m.idx)..O_EXT, "a")
-                FD_O:write(m.lib, '\n')
-                FD_O:write(parsed_line.original, '\n')
-
-                m.written = true
-
-            end
-
-            if FD_O ~= nil then FD_O:close() end
-            FD_O = io.open(OBJ_PATH..string.format(".%03d", m.idx)..O_EXT, "a")
-
-        elseif type == "f" then
-
-            local prim_str = "f "
-            -- Processing each triangle separately
-            for i=1, #(parsed_line.f) do
-
-                local v = parsed_line.f[i]
-
-                prim_str = prim_str..v.vi.."/"
-                if CONVERT_MATERIALS then prim_str = prim_str..v.vti end
-                prim_str = prim_str.."/"
-                if CONVERT_NORMALS then prim_str=prim_str..v.vni end
-                prim_str = prim_str.." "
-
-            end
-
-            -- Writing string to file
-            FD_O:write(prim_str, '\n')
-
-        end
-
-    end
-
-    if FD_O ~= nil then FD_O:close() end
-    if FD_MT ~= nil then FD_MT:close() end
-
-    os.execute("rm "..OBJ_PATH..TO_EXT)
-
-end
-
-
-transformObj = function ()
-
-    if SPLIT_OBJ then
-
-        splitObj()
-
-    else
-
-        stripObj()
-
-    end
-
-end
 
 -- Parses the program arguments
 --- FORMAT: lua obj_converter.lua [FLAGS] file.obj
@@ -309,11 +162,7 @@ parseArguments = function (arg)
 
     for i=1, #(arg) - 1 do
 
-        if arg[i] == "-ns" then
-
-            SPLIT_OBJ = false
-
-        elseif arg[i] == "-nn" then
+        if arg[i] == "-nn" then
 
             CONVERT_NORMALS = false
 
@@ -325,13 +174,6 @@ parseArguments = function (arg)
 
     end
 
-    if not CONVERT_MATERIALS and SPLIT_OBJ then
-
-        print("Can't use both -nm and -ns flags (i.e., can't split an .obj by material and strip the materials)")
-        os.exit(1)
-
-    end
-
     OBJ_PATH = arg[#arg]
 
 end
@@ -340,4 +182,4 @@ end
 parseArguments(arg)
 
 -- Converting file
-transformObj()
+stripObj()
